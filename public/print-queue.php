66b73d2cb90edad7f66f8600a68546441dff590e
BERT151 <102898303+BERT151@users.noreply.github.com>
2024-01-26 21:48:52 +0800

    Create print-queue.php

---
+++ public/print-queue.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<head>
+    <title>Queuing ticket</title>
+    <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Benne' rel='stylesheet'>
+    
+    <style>
+    .bg{
+        background-color:white;
+        background-size: 100%;
+        height: 100vh;
+        background-repeat: no-repeat;
+    }
+    
+    
+    .QCLOGO{
+        height: 100px;
+        width: 120px;
+    }
+    body {
+        margin: 0;
+        font-size: 35px;
+        color:black;
+        font-weight: bolder;
+        font-family: 'Inter';
+    }
+    h1
+    {
+        color: black;
+        text-align: center;
+        font-size: 190px;
+        font-family:Arial, Helvetica, sans-serif    ;
+        margin-top: 0px;
+        margin-bottom: 0px;
+    }
+   h2{ color: white;
+        text-align: center;
+        font-family: 'Glacial Indifference', sans-serif;                               
+        font-size: 70px;
+        font-weight: bold;
+   }
+    h3{
+        color: black;
+        text-align: center;
+        font-family: 'Glacial Indifference', sans-serif;                               
+        font-size: 30px;
+        font-weight: bold;
+    }
+    h4{
+        margin-top: -290px;
+        color: black;
+        font-size: 35px;
+        font-weight: bolder;
+        font-family: 'Inter';
+    }
+    
+    
+    
+    .taas {
+        display: flex;
+        flex-direction: column; 
+        align-items: center ;
+    }
+    
+    ul {
+        display: flex;
+    }
+    
+    li {
+        list-style: none;
+    }
+    
+   
+
+      .buttons{
+        float: right;
+        margin-top: -100px;
+      }
+       
+      
+     
+p{
+        margin-top: -230px;
+        color: black;
+        font-size: 35px;
+        font-weight: bolder;
+        font-family: 'Inter';
+        margin-left: -400px;
+
+}
+
+
+    </style>
+</head>
+
+<body>
+    
+
+    <div class="bg">
+        <div class="taas">
+        <ul> 
+            <li><img class="QCLOGO" src="./assets/images/qclogo.jpg"></li>
+            <li><pre><h3> Quezon City Public Library
+    Quezon City Goverment  
+            </h3></pre></li>
+            <li><img class="QCLOGO" src="./assets/images/qcpl.logo.png"></li>
+        
+        </ul> 
+       
+</div>
+<center>
+    <br><br><br><br>
+
+    
+    <p>QUEUE NUMBER:</p>
+    
+    <br>
+    <h1><b>P-0000</b></h1>
+    
+    <p2>22-JAN-2023 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;11:43 PM</p2>
+    <br><br><br>
+    <p3><i>You will be served based on your<br>Queue Number</i></p3>
+ 
+</center>
+<br><br><br><br>
+<div class=" buttons">
+    
+</div>
+</html>
+
+    
+
+
+</body>
+</html> 
